ed73f8e42718977d39a0e441ee275eeddca6225f
Leandro Nares <leandro.nares@inmetrics.com.br>
2021-03-15 19:04:43 -0300

    Correção do TODO na TasksRepository.

--- pact-frontend/src/main/java/br/ce/wcaquino/tasksfrontend/repositories/TasksRepository.java
+++ pact-frontend/src/main/java/br/ce/wcaquino/tasksfrontend/repositories/TasksRepository.java
@@ -24,17 +24,17 @@ public class TasksRepository {
 	
 	public void update(Todo todo) {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.put(url + "/todo"+ todo.getId(),todo);
+		restTemplate.put(url + "/todo/"+ todo.getId(),todo);
 	}
 	
 	public void delete(Long id) {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.delete(url + "/todo"+ id);
+		restTemplate.delete(url + "/todo/"+ id);
 	}
 	
 	public Todo getTodo(Long id) {
 		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.getForObject(url + "/todo"+ id, Todo.class);
+		return restTemplate.getForObject(url + "/todo/"+ id, Todo.class);
 	}
 	
 }
